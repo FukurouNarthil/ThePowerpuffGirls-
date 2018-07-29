62f32247537c0328981b2a847af7547b1da424b7
FukurouNarthil <cucnarthil@153.com>
2018-07-30 05:40:34 +0800

    decrypt

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, json, redirect, url_for, send_from_directory, abort, jsonify, flash, session, logging
+#coding=utf-8
+
+from flask import Flask, render_template, request, json, redirect, url_for, send_from_directory, abort, jsonify, flash, session, logging, send_file
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from Crypto import Random
 from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
@@ -9,6 +11,14 @@ import os
 from werkzeug import secure_filename
 from OpenSSL.crypto import load_privatekey, FILETYPE_PEM, sign
 import MySQLdb
+import datetime
+import sys
+import random
+import re
+import rsa
+import base64
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
 
 UPLOAD_FOLDER = 'files'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'xls', 'ppt'])
@@ -48,30 +58,106 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
+class prpcrypt():
+    def __init__(self, key):
+        self.key = key
+        self.mode = AES.MODE_CBC
+	self.iv = os.urandom(16)
+
+
+    def encrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.iv)
+        
+        length = 32
+        count = len(text)
+        if (count % length != 0):
+            add = length - (count % length)
+        else:
+            add = 0
+        text = text + ('\0' * add)
+        self.ciphertext = cryptor.encrypt(text)
+        
+       
+        return b2a_hex(self.ciphertext)
+
+    
+    def decrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.iv)
+        plain_text = cryptor.decrypt(a2b_hex(text))
+        return plain_text.rstrip('\0')
+
+
 @app.route('/upload', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
+	    filename = secure_filename(file.filename)
+	    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	    if 'id' in session:
+		# session['id'] = session['id'][0]
 		print(session['id'])
+	    else:
+		print("no session id")
+	    
+            now_time = datetime.datetime.now()  
+
+            
+            fsize = os.path.getsize(UPLOAD_FOLDER)
+            fsize = fsize / float(1024 * 1024) 
+
 	    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="123456", db="acdemo")
             cursor = conn.cursor()
-            cursor.callproc('sp_createFile', (file.filename, 'key', 'sha', 1)) 
+            file = request.files['file']
+            f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "rb+")
+            data = f.read()
+	    f.close()
+            
+            hash = hashlib.sha256()
+            hash.update(data)
+            hashed_data = hash.hexdigest()
+
+            
+            key = ''  
+            base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789'  
+            length = len(base_str) - 1
+            for i in range(32):
+                key += base_str[random.randint(0, length)]
+	 
+	    # encrypt file
+            pc = prpcrypt(key)  
+            e_data = pc.encrypt(data)
+	    f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "w")
+	    f.write(e_data)
+	    f.close()
+	    
+	    # encrypt key
+            cursor.execute("SELECT pubkey FROM users where users.id = (%s)", [session['id']])
+            r_pubkey = cursor.fetchall()[0]
+	    rsakey = RSA.importKey(r_pubkey)
+	    cipher = Cipher_pkcs1_v1_5.new(rsakey)
+	    enckey = base64.b64encode(cipher.encrypt(key))
+
+	    print("%s %s", filename, fsize)
+	   
+            cursor.callproc('sp_createFile', (
+                filename,  
+                fsize, 
+                enckey, 
+                hashed_data,  
+                session['id'],  
+                now_time))  
             data = cursor.fetchall()
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload',
-                                    filename=filename))
-   
-    flash('File successfully uploaded!')
+	    if len(data) is 0:
+            	flash('File successfully uploaded!')
+            	conn.commit()
+		return '''<h2>You've upload it successfully!</h2>'''
     return render_template('upload.html')
 
 
 def generateKey():
-    random_generator = Random.new().read
 
-    rsa = RSA.generate(1024, random_generator)
+    rsa = RSA.generate(1024)
 
     private_pem = rsa.exportKey()
     public_pem = rsa.publickey().exportKey()
@@ -116,7 +202,8 @@ def toSignUp():
 	print(data)
 
         if len(data):
-	    session['id'] = data
+	    session['id'] = data[0][0]
+	    print(session['id'])
             conn.commit()
             return redirect(upDown_url) 
         else:
@@ -143,12 +230,10 @@ def toSignIn():
         cursor = conn.cursor()
         cursor.callproc('sp_userLogin', (_name, hashed_password))
         data = cursor.fetchall()
-	session['id'] = data
-	session['user'] = _name
+	session['id'] = data[0][0]
 	
 	print(data)
 	print(session['id'])
-	print(session['user'])	
 	if data:
 	    flash('Log in successfully!')
 	    return redirect(upDown_url) 
@@ -161,37 +246,53 @@ def toSignIn():
 def list_files():
     """Endpoint to list files on the server."""
     files = []
-    for filename in os.listdir(UPLOAD_FOLDER):
-        path = os.path.join(UPLOAD_FOLDER, filename)
-        if os.path.isfile(path):
-            files.append(filename)
-    return jsonify(files)
+    if 'id' in session:
+	conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="123456", db="acdemo")
+        cursor = conn.cursor()
+	
+        for filename in os.listdir(UPLOAD_FOLDER):
+	    print(filename)
+            path = os.path.join(UPLOAD_FOLDER, filename)
+	    cursor.execute("SELECT uid FROM files where files.name = (%s)", [filename])
+            data = cursor.fetchall()
+	    
+	    for i in list(data):
+		print(list(i))
+                if session['id'] in list(i) and os.path.isfile(path):
+                    files.append(filename)
+	            print(files)
+	cursor.close()
+    return render_template('download.html', files=files)
 
 
-@app.route('/download/<path:path>')
-def get_file(path):
+@app.route('/download/<path:filename>')
+def get_file(filename):
     """Download a file."""
-    return send_from_directory(UPLOAD_FOLDER, path, as_attachment=True)
-
-
-@app.route('/download/<filename>', methods=['POST'])
-def post_file(filename):
-    """Upload a file."""
-
-    if '/' in filename:
-        # Return 400 BAD REQUEST
-        abort(400, 'no subdirectories directories allowed')
-
-    with open(os.path.join(UPLOAD_FOLDER, filename), 'wb') as fp:
-        fp.write(request.data)
-
-    # Return 201 CREATED
-    return '', 201
-
+    if 'id' in session:
+        conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="123456", db="acdemo")
+        cursor = conn.cursor()
+	cursor.execute("SELECT privkey FROM users WHERE users.id = (%s)", [session['id']])
+        privkey = cursor.fetchall()[0][0]
+	cursor.execute("SELECT enckey FROM files WHERE files.name = (%s) and files.uid = (%s)", [filename, session['id']])
+	enckey = cursor.fetchall()[0][0]
+       
+	f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "wb+")
+	data = f.read()
+	# decrypt aes key
+	rsakey = RSA.importKey(privkey)
+	cipher = Cipher_pkcs1_v1_5.new(rsakey)
+	# decrypt file
+	random_generator = Random.new().read
+	key = cipher.decrypt(base64.b64decode(enckey), random_generator)
+	pc = prpcrypt(key)  
+        text = pc.decrypt(data)
+	f.write(text)
+	f.close()
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 
 if __name__ == '__main__':
     app.secret_key = 'secretkey'
-    app.run(host='0.0.0.0', debug="True", threaded=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', debug="True", threaded="True", ssl_context=('cert.pem', 'key.pem'))
 
 
